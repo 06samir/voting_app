67f7a219d77f528d0093c43fe01196c5866f4834
Sameer Lalloti <lallotisameer@gmail.com>
2026-03-13 18:20:18 +0530

    first version of our voting app

---
+++ db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose'); 
+
+mongoose.connect("mongodb://127.0.0.1:27017/voting_app")
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.log("Database error:", err.message));
+
+  const db = mongoose.connection;
+
+  db.on('connected', () => {
+    console.log('DB is connected')
+  })
+
+  db.on('dis-connected', () => {
+    console.log('DB is connected')
+  })
+  
+  db.on('error', (err) => {
+    console.log('DB is connected')
+  })
+
+  module.exports = db;
